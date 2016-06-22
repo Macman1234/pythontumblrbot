5d37ae88bcb75bfca54037640c1a3ce2c57f1c31
Macman1223 <ninjamurloc@gmail.com>
2016-06-21 21:35:42 -0400

    add in comments explaining to friend

--- testmaketextpost.py
+++ testmaketextpost.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
-import pytumblr
-keyfile = open("/home/maclean/keys.txt", "r+")
-str = keyfile.read(204)
-keys = str.split( )
+import pytumblr # installs the library that makes talking to Tumblr possible through this coding
+keyfile = open("/home/maclean/keys.txt", "r+") # opens the file containing all the security stuff
+str = keyfile.read(204) # using that file object, makes a text (a string) object
+keys = str.split( ) # seperates all 4 keys (first two have to do with the API itself or something,
+# second half have to do with my specific account)
 
-client = pytumblr.TumblrRestClient(
+client = pytumblr.TumblrRestClient( # starts the stuff for talking to Tumblr
     keys[0],
     keys[1],
     keys[2],
     keys[3],
 )
 
-print(client.blog_info('macmarkov'))
+print(client.blog_info('macmarkov')) # outputs the info of the blog to my screen when I run it (just for debugging)
 
 client.create_text("macmarkov", state="published", slug="test-post", title="test", body="test post please ignor")
+# ^ actually makes the post
