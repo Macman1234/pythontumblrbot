cfeda2ded25c95a0393e750069b5a8010f98b80d
Macman1223 <ninjamurloc@gmail.com>
2016-06-21 21:21:25 -0400

    forgot to make fileio reflect actual char length
    should probably read the file length and then the whole file

--- testmaketextpost.py
+++ testmaketextpost.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import pytumblr
 keyfile = open("/home/maclean/keys.txt", "r+")
-str = keyfile.read(102)
+str = keyfile.read(204)
 keys = str.split( )
 
 client = pytumblr.TumblrRestClient(
@@ -11,4 +11,4 @@ client = pytumblr.TumblrRestClient(
     str[3],
 )
 
-client.create_text("macmarkov", state="published", slug="huh-did-not-know-you-could-do-this", title="test", body="test post please ignor")
+client.create_text("macmarkov", state="published", slug="test-post", title="test", body="test post please ignor")
