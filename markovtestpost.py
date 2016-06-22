579fc78ac31e0df6295fd669efc8912a4abe6176
Macman1223 <ninjamurloc@gmail.com>
2016-06-21 22:17:44 -0400

    hdgaghda

--- markovtestpost.py
+++ markovtestpost.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import pytumblr # installs the library that makes talking to Tumblr possible through this coding
+import markov
 keyfile = open("/home/maclean/keys.txt", "r+") # opens the file containing all the security stuff
 str = keyfile.read(204) # using that file object, makes a text (a string) object
 keys = str.split( ) # seperates all 4 keys (first two have to do with the API itself or something,
