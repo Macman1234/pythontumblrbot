a0aec1a62c5a3051829a9d8bfe8dcec81bac6556
Maclean Parisi <ninjamurloc@gmail.com>
2016-07-30 13:51:55 -0400

    added in thermal printer asdf

---
+++ markovmakepostprint.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import pytumblr # installs the library that makes talking to Tumblr possible through this coding
+import markov
+import os
+import random
+keyfile = open("~/pythonresources/keys.txt", "r+") # opens the file containing all the security stuff
+str = keyfile.read(204) # using that file object, makes a text (a string) object
+keys = str.split( ) # seperates all 4 keys (first two have to do with the API itself or something,
+# second half have to do with my specific account)
+
+client = pytumblr.TumblrRestClient( # starts the stuff for talking to Tumblr
+    keys[0],
+    keys[1],
+    keys[2],
+    keys[3],
+)
+
+print("updating Steve")
+
+genfile = open("~/pythonresources/genfile.txt", "r+")
+
+asklist = client.posts('macmarkov')
+
+for ask in asklist['posts']:
+    if ask['type'] == 'answer':
+        genfile.write(ask['question'] + '\n')
+
+genfile.close()
+
+markov.Parse("askgen", 2, "~/pythonresources/genfile.txt")
+
+asklist = client.submission('macmarkov')
+
+for ask in asklist['posts']:
+    if ask['type'] == 'answer':
+        if ask['state'] == 'submission':
+            generated = markov.Gen("askgen", random.randrange(4, 6, 1))
+            client.edit_post('macmarkov', id=ask['id'], answer=generated; # edit a post
+            os.system("echo " + generated + ' | lpr')
+
+os.remove('askgen.db')
